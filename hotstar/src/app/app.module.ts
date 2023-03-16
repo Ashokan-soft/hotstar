f094d3ef80198bf578e8801281f2e1f2c513861d
Ashok <ashokansoft@gmail.com>
2023-03-16 15:53:17 +0530

    project creation

---
+++ hotstar/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    { provide: LocationStrategy, useClass:HashLocationStrategy}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
